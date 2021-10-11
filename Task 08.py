9418508246b61b4296af2a02e68ab90ea4f96396
petersvl <91634202+petersvl@users.noreply.github.com>
2021-10-11 13:12:45 +0200

    Add files via upload

--- Task 08.py
+++ Task 08.py
@@ -1,13 +1,20 @@
 #Task 0.8
 
-def covert_to_time(num):
+def convert_to_time(num):
     hour1 = num//60
     minute = num%60
-    if hour1 > 1 and minute > 0:
-        print(hour1, "hours", minute, "minutes")
-    else:
+    if hour1 == 1 and minute == 1:
+        print(hour1, "hour", minute, "minute")
+    elif hour1 > 1 and minute == 1:
+        print(hour1, "hours", minute, "minute")
+    elif hour1 == 1 and minute > 1:
         print(hour1, "hour", minute, "minutes")
+    elif hour1 > 1 and minute > 1:
+        print(hour1, "hours", minute, "minutes")
 
-covert_to_time(100)
-covert_to_time(310)
+
+convert_to_time(61)
+convert_to_time(100)
+convert_to_time(181)
+convert_to_time(310)
 
