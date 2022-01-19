ab3e0978f17cc3a804791942704217733c1b305c
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-01-19 15:31:15 +0200

    Update Task 09.py

--- Task 09.py
+++ Task 09.py
@@ -1,11 +1,11 @@
 #Task 0.9
 
-def vowels_only():
-    word = "Olivia"
-    list_vowels = ("a", "e", "i", "o", "u")
+def vowels_only(word):
+    list_vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
     for i in word:
         if i not in list_vowels:
             word = word.replace(i, " ")
     print(word)
 
-vowels_only()
+vowels_only("Olivia")
+vowels_only("Umuzi")
