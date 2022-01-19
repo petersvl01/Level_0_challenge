18ae8454ab2904fcf2a6bdf471872c1e7a6e853f
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-01-19 15:21:17 +0200

    Update Task 05.py

--- Task 05.py
+++ Task 05.py
@@ -1,11 +1,8 @@
 #Task 0.5
 
-def area_of_triangle():
-    a = 10
-    b = 20
-    c = 20
-    s = ((a + b + c)/2)
-    area = (s*(s-a) * (s-b) * (s-c))**0.5
+def area_of_triangle(a, b, c):
+    s = ((a + b + c) / 2)
+    area = (s*(s - a) * (s - b) * (s - c))**0.5
     print(area)
-    
-area_of_triangle()
+
+area_of_triangle(6, 4, 3)
