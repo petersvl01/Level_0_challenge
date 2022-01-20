8401967d920f02f5f4137cb186692c0653c70934
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-01-20 14:08:04 +0200

    Update Task 04.py

--- Task 04.py
+++ Task 04.py
@@ -1,13 +1,13 @@
 #Task 0.4
 
 def even_or_odd(num):
-    remainder = num % 2
-    if (remainder == 0):
-        print("even")
-    else:
-        print("odd")
+  remainder = num % 2
+  if (remainder == 0):
+    print("even")
+  else:
+    print("odd")
 
-(even_or_odd(3))
 
-(even_or_odd(4))
+even_or_odd(2)
+even_or_odd(7)
 
