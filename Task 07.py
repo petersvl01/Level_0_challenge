383c3afd01b7b92f9a09708b9185324caec9e758
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-01-20 14:11:26 +0200

    Update Task 07.py

--- Task 07.py
+++ Task 07.py
@@ -1,15 +1,15 @@
 #Task 0.7
 
-def covert_to_Fahrenheit(x):
-    Celsius = x
-    Fahrenheit = (Celsius * 1.8) + 32
-    print("%.2f Celsius = %.2f Fahrenheit" %( Celsius, Fahrenheit))
+def covert_to_fahrenheit(x):
+    celsius = x
+    fahrenheit = (celsius * 1.8) + 32
+    print("%.2f celsius = %.2f fahrenheit" %( celsius, fahrenheit))
 
-covert_to_Fahrenheit(13)
+covert_to_fahrenheit(13)
 
-def covert_to_Celsius(y):
-    Fahrenheit = y
-    Celsius = (Fahrenheit - 32) * 0.556
-    print("%.2f Fahrenheit = %.2f Celsius" %( Fahrenheit, Celsius))
+def covert_to_celsius(y):
+    fahrenheit = y
+    celsius = (fahrenheit - 32) * 0.556
+    print("%.2f fahrenheit = %.2f celsius" %( fahrenheit, celsius))
 
-covert_to_Celsius(50)
+covert_to_celsius(50)
