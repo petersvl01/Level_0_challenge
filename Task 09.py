e8df754906882d93e595d764a58d9d31905fe3a9
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-18 01:55:00 +0200

    Update Task 09.py

--- Task 09.py
+++ Task 09.py
@@ -1,22 +1,14 @@
 #Task 0.9
 
 def vowels_only(word):
-    list_vowels = ("a", "e", "i", "o", "u")
-    for i in word:
-        if i not in list_vowels:
-            word = word.lower()
-            word = word.replace(i, " , ")
-    print(word)
+    LIST_VOWELS = ['a', 'e', 'i', 'o', 'u']
+    word = word.lower()
+    find_vowels = []
+    for char in word:
+        if char in LIST_VOWELS:
+            if char not in find_vowels:
+                find_vowels.append(char)
+                newstr = ', '.join(find_vowels)
+    print("Vowels: ",newstr)
 
-vowels_only("Umuzi")
-
-#or,
-
-def vowels_only(word1):
-    list_vowels = ("a", "e", "i", "o", "u", "A", "E", "I", "O", "U")
-    for j in word1:
-        if j not in list_vowels:
-           word1 = word1.replace(j, " , ")
-    print(word1)
-
-vowels_only("Olive")
+vowels_only("Airbase")
