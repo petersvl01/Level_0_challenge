0dd746bb57daaae9b9c82aabf7058a16380632b6
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-18 01:58:04 +0200

    Update Task 10.py

--- Task 10.py
+++ Task 10.py
@@ -1,8 +1,6 @@
 def common_words(word_1, word_2):
-    word_1 = word_1.lower()
-    word_2 = word_2.lower()
-    s1 = set(word_1)
-    s2 = set(word_2)
+    s1 = set(word_1.lower())
+    s2 = set(word_2.lower())
     list = (s1 & s2)
     for char in list:
         char = ', '.join(list)
