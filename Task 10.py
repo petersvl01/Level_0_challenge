96ac6e8405220092b49de4d250ecff68ca079b63
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-17 12:04:40 +0200

    Update Task 10.py

--- Task 10.py
+++ Task 10.py
@@ -1,8 +1,12 @@
-def common_words(x, y):
-    s1 = set(x)
-    s2 = set(y)
+def common_words(word_1, word_2):
+    word_1 = word_1.lower()
+    word_2 = word_2.lower()
+    s1 = set(word_1)
+    s2 = set(word_2)
     list = (s1 & s2)
-    print("Common letter: {}" .format(list))
-
-common_words("house", "computer")
-common_words("House", "Hose")
+    for char in list:
+        char = char.lower()
+        char = ', '.join(list)
+    print("Common characters: ",char)
+   
+common_words("Never", "Ever")
