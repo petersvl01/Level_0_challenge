b98bb49aba1982ac6c8543003cc284bd374c3358
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-18 02:14:47 +0200

    Update Task 09.py

--- Task 09.py
+++ Task 09.py
@@ -1,12 +1,9 @@
-#Task 0.9
-
 def vowels_only(word):
     LIST_VOWELS = ['a', 'e', 'i', 'o', 'u']
     word = word.lower()
     find_vowels = []
     for char in word:
-        if char in LIST_VOWELS:
-            if char not in find_vowels:
+        if char in LIST_VOWELS and char not in find_vowels:
                 find_vowels.append(char)
                 newstr = ', '.join(find_vowels)
     print("Vowels: ",newstr)
