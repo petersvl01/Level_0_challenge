c8eae11f36aef0e13c25e0e9d8853dde3610af87
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-18 09:31:31 +0200

    Update Task 05.py

--- Task 05.py
+++ Task 05.py
@@ -1,6 +1,6 @@
 def area_of_triangle(a, b, c):
-    s = ((a + b + c) / 2)
-    area = (s*(s - a) * (s - b) * (s - c))**0.5
+    semiperimeter = ((a + b + c) / 2)
+    area = ( semiperimeter*( semiperimeter - a) * ( semiperimeter - b) * ( semiperimeter - c))**0.5
     print(area)
 
 area_of_triangle(6, 4, 3)
