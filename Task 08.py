16e75505d69b5d301028fbf1885784b651bac6f0
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-23 19:50:13 +0200

    Update Task 08.py

--- Task 08.py
+++ Task 08.py
@@ -1,17 +1,17 @@
 def convert_to_time(num):
     minutes1 = num % 60
     hour1 = num // 60
-    if hour1 <= 1:
+    if hour1 == 1:
         hour =" hour"
     else:
         hour =" hours"
-    if minutes1 <= 1:
+    if minutes1 == 1:
         minute =" minute"
     else:
         minute =" minutes"
     print(f'{hour1} {hour}, {minutes1} {minute}')
       
 
-convert_to_time(100)
-convert_to_time(181)
+convert_to_time(40)
+convert_to_time(60)
 convert_to_time(310)
