598a302527f2b687f76e01e643eb75793ebc722f
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-23 18:27:14 +0200

    Update Task 10.py

--- Task 10.py
+++ Task 10.py
@@ -1,4 +1,4 @@
-def common_words(word_1, word_2):
+def common_Letters(word_1, word_2):
     s1 = set(word_1.lower())
     s2 = set(word_2.lower())
     list = (s1 & s2)
@@ -6,5 +6,5 @@ def common_words(word_1, word_2):
         char = ', '.join(list)
     print("Common characters: ",char)
    
-common_words("Never", "Ever")
+common_Letters("Never", "Ever")
 
