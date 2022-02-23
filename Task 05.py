bdf56ef1eca764262c4123ad2f2b429fc64285c3
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-23 20:12:30 +0200

    Update Task 05.py

--- Task 05.py
+++ Task 05.py
@@ -1,6 +1,6 @@
 def area_of_triangle(side_1, side_2, side_3):
     semiperimeter = ((side_1 + side_2 + side_3) / 2)
     area = ( semiperimeter*( semiperimeter - side_1) * ( semiperimeter - side_2) * ( semiperimeter - side_3))**0.5
-    print(area)
+    return area
 
 area_of_triangle(6, 4, 3)
