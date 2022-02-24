60768c4ed8f8985b0a4adace0b74844b64380b5d
petersvl01 <95767636+petersvl01@users.noreply.github.com>
2022-02-24 16:49:28 +0200

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -1,9 +1,9 @@
-def find_max(*nums):
-    max_num = nums[0]
+def find_maximum_number(*nums):
+    max_number = nums[0]
     for num in nums:
-        if num > max_num:
-            max_num = num
-    return max_num
+        if num > max_number:
+            max_number = num
+    return max_number
 
-print(find_max(20, -6, 1, 9))
-print(find_max(20, -6, 40, 1, 9))
+print(find_maximum_number(20, -6, 1, 9))
+print(find_maximum_number(20, -6, 40, 1, 9))
